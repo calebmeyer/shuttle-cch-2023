66fa39f5797c648de3591ab2a86841e3fbfdc417
Caleb Meyer <caleb.meyer@hy-vee.com>
2023-11-27 17:58:01 -0600

    Update for challenge

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
-use rocket::{get, routes};
+use rocket::{get, http::Status, routes};
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/-1/error")]
+fn error() -> Status {
+    Status::InternalServerError
+}
 #[shuttle_runtime::main]
 async fn main() -> shuttle_rocket::ShuttleRocket {
-    let rocket = rocket::build().mount("/", routes![index]);
+    let rocket = rocket::build().mount("/", routes![index, error]);
 
     Ok(rocket.into())
 }
