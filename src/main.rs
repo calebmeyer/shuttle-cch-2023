6a8a9a3a70cf176102890388f2837acb21ae34ac
Caleb Meyer <caleb.meyer@hy-vee.com>
2023-12-21 23:06:21 -0600

    Get day 1 + bonus

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,23 @@
-use rocket::{get, http::Status, routes};
+use std::num::ParseIntError;
+
+use rocket::{get, http::Status, request::FromSegments, routes};
+
+struct PathNumberSegments(Vec<i32>);
+
+impl<'r> FromSegments<'r> for PathNumberSegments {
+    type Error = ParseIntError;
+
+    fn from_segments(
+        segments: rocket::http::uri::Segments<'r, rocket::http::uri::fmt::Path>,
+    ) -> Result<Self, Self::Error> {
+        Ok(PathNumberSegments(
+            segments
+                .into_iter()
+                .map(|num| num.parse())
+                .collect::<Result<Vec<i32>, Self::Error>>()?,
+        ))
+    }
+}
 
 #[get("/")]
 fn index() -> &'static str {
@@ -9,9 +28,20 @@ fn index() -> &'static str {
 fn error() -> Status {
     Status::InternalServerError
 }
+
+#[get("/1/<numbers..>")]
+fn day1(numbers: PathNumberSegments) -> String {
+    numbers
+        .0
+        .into_iter()
+        .reduce(|acc, el| acc ^ el)
+        .unwrap_or_default()
+        .pow(3)
+        .to_string()
+}
 #[shuttle_runtime::main]
 async fn main() -> shuttle_rocket::ShuttleRocket {
-    let rocket = rocket::build().mount("/", routes![index, error]);
+    let rocket = rocket::build().mount("/", routes![index, error, day1]);
 
     Ok(rocket.into())
 }
